fac1e07af11da7d5d55127d4a4909bc98245adeb
Galabin Vasilev <galabinvasilev@Galabins-MacBook-Pro.local>
2025-08-13 16:38:47 +0300

    Add NuxtHub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui-pro'],
+  modules: ['@nuxt/eslint', '@nuxt/ui-pro', '@nuxthub/core'],
 
   css: ['~/assets/css/main.css'],
 
